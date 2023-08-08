19f29620610d7bc224e10083318b0718e4e1ca77
melxiory <sibagatullinnail@gmail.com>
2023-08-08 14:55:41 +0300

    create site Blog!!!

--- app/js/main.js
+++ app/js/main.js
@@ -18,6 +18,10 @@ $(function () {
     slidesToShow: 10,
     slidesToScroll: 10,
   });
+  $('.article-slider__box').slick({
+    prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-left"><img src="images/arrow-slide-left.svg" alt="arrow slide left"></button>',
+    nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrow-right"><img src="images/arrow-slide-right.svg" alt="arrow slide right"></button>',
+  });
 
   var mixer = mixitup('.gallery__inner', {
     load:{
